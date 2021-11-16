128e3a0fa37270048f4c833d8abb13f51cfd1e80
Eun Suh Sung <esung2@nd.edu>
2021-11-16 16:19:08 -0500

    Exericse 09 as of 16/11

---
+++ Ex9.R
@@ -0,0 +1,21 @@
+setwd("~/Desktop/r-novice-inflammation/Ex9/")
+
+##Custom function
+##
+function <- function(dir, col){   #dir = directory col = specified column #obs=50, default needs 50 obs.
+  
+  if (ncol()<50){
+    print(Warning: File has less than 50 observations)
+  }else{
+    for (file in list.files(dir)){
+      ##print CV for the specified column
+    }#end for loop
+  } #end of if-else 
+} #end custom function
+  
+#needs to loop through every file in the directory
+for (file in list.files()){
+  print()
+}
+  
+#coefficient of variation: CV <- sd(x)/mean(x)
