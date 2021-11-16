4645ae7e240eb3466c07d600727ded379b6e6554
Eun Suh Sung <esung2@nd.edu>
2021-11-16 16:27:45 -0500

    Ex. 9 as of 16/11

--- Ex9.R
+++ Ex9.R
@@ -1,14 +1,14 @@
 setwd("~/Desktop/r-novice-inflammation/Ex9/")
 
 ##Custom function
-##
-function <- function(dir, col){   #dir = directory col = specified column #obs=50, default needs 50 obs.
+
+function <- function(dir, col){   #dir = directory col = specified column
   
-  if (ncol()<50){
+  if (ncol()<50){ ##would it be ncol??? or maybe nrow after subsetting the column
     print(Warning: File has less than 50 observations)
   }else{
     for (file in list.files(dir)){
-      ##print CV for the specified column
+      ##print CV for the specified column in each file
     }#end for loop
   } #end of if-else 
 } #end custom function
