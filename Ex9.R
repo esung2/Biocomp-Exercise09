09fa95fb52acd6a4ba9ea9b6020d5bff204276cb
Eun Suh Sung <esung2@nd.edu>
2021-11-17 15:01:59 -0500

    Ex. 9 as of 17/11

--- Ex9.R
+++ Ex9.R
@@ -2,20 +2,30 @@ setwd("~/Desktop/r-novice-inflammation/Ex9/")
 
 ##Custom function
 
+#custom function
 function <- function(dir, col){   #dir = directory col = specified column
+  results <- 0
+  for (i in list.files(dir)){
+   table <- read.table(file=paste(dir,i,sep=""), sep=",", header = TRUE, stringsAsFactors=FALSE)  #assuming files are all csv files
+    if (nrow(table)<50){
+      #add if-else for override
+      print("Warning: There are less than 50 observations")
+      #userinput code
+      if(override==true){
+        results <- c(results, sd(table[,col])/mean(table[,col]))
+      }else{
+        results <- c(results, NA)
+      }
+    }else{
+      results <- c(results, sd(table[,col])/mean(table[,col]))
+    } #end if-else
+  } #end for loop
+  return(results[-1]) #to remove 0 from vector
+} #end custom function  
+    
+file[,col]
   
-  if (ncol()<50){ ##would it be ncol??? or maybe nrow after subsetting the column
-    print(Warning: File has less than 50 observations)
-  }else{
-    for (file in list.files(dir)){
-      ##print CV for the specified column in each file
-    }#end for loop
-  } #end of if-else 
-} #end custom function
-  
-#needs to loop through every file in the directory
-for (file in list.files()){
-  print()
-}
+#needs to loop through every file in the directory to read data from each file 
+
   
 #coefficient of variation: CV <- sd(x)/mean(x)
