d9d5246b1d4740ace8134a1133a83381c2d8c2b1
Eun Suh Sung <esung2@nd.edu>
2021-11-18 19:54:28 -0500

    Ex. 9 as of 18/11

--- Ex9.R
+++ Ex9.R
@@ -1,17 +1,15 @@
-setwd("~/Desktop/r-novice-inflammation/Ex9/")
+setwd("~/Desktop/r-novice-inflammation/")
 
 ##Custom function
 
-#custom function
-function <- function(dir, col){   #dir = directory col = specified column
+function <- function(dir, col){   #dir = specified directory, col = specified column
   results <- 0
   for (i in list.files(dir)){
    table <- read.table(file=paste(dir,i,sep=""), sep=",", header = TRUE, stringsAsFactors=FALSE)  #assuming files are all csv files
     if (nrow(table)<50){
-      #add if-else for override
-      print("Warning: There are less than 50 observations")
-      #userinput code
-      if(override==true){
+      print("Warning: There are less than 50 observations.")
+      override <- readline("If you would still like to continue, press Y. To exit, press any other key. ")
+      if(override=="Y"){
         results <- c(results, sd(table[,col])/mean(table[,col]))
       }else{
         results <- c(results, NA)
@@ -22,10 +20,3 @@ function <- function(dir, col){   #dir = directory col = specified column
   } #end for loop
   return(results[-1]) #to remove 0 from vector
 } #end custom function  
-    
-file[,col]
-  
-#needs to loop through every file in the directory to read data from each file 
-
-  
-#coefficient of variation: CV <- sd(x)/mean(x)
